1504c6ec2b5aa9c6d6146d79f92d0b1738ce32f0
MotazAh <almotaz.ahmed@gmail.com>
2023-02-23 20:36:00 +0000

    Modified get_ref.py for optimization

--- get_ref.py
+++ get_ref.py
@@ -40,52 +40,71 @@ def get_image_similarity(img1, img2):
 
   return calculate_similarity(feature_v1, feature_v2)
 
-# Get top 3 similar images to an image from a dataset folder
-def find_top_images(img, img_dir_path):
-  top1_score = 0
+# Get top 2 similar images from a dataset folder
+def find_top_images(img, feature_dir_path):
+  top_score = 0
   top2_score = 0
-  top3_score = 0
   counter = 0
 
-  img_dir = os.listdir(img_dir_path)
-  print("File count = " + str(len(img_dir)))
+  feature_dir = os.listdir(feature_dir_path)
+  print("File count = " + str(len(feature_dir)))
 
   # Feature vector of image to be compared with dataset
   img_vect = get_feature_vector(img)
 
+  for file_name in feature_dir:
+    counter += 1
+    if (counter % 100) == 0:
+      print(counter)
+
+    # Gets image file name without .txt in the end
+    file_path = os.path.join(feature_dir_path, file_name)
+    img_ref_vect = parse_feature_file(file_path)
+
+    score = calculate_similarity(img_vect, img_ref_vect)
+
+    if score > top_score:
+      top_image_name = file_name[:-4]
+      top_score = score
+    elif score > top2_score:
+      top2_image_name = file_name[:-4]
+      top2_score = score
+
+  top_image_paths = [top_image_name, top2_image_name]
+  return top_image_paths
+
+# Get feature vectors for all images in an image dir
+def get_feature_vectors(img_dir_path):
+  img_dir = os.listdir(img_dir_path)
+  counter = 0
+
   for file_name in img_dir:
     file_path = os.path.join(img_dir_path, file_name)
-    img_ref = cv2.imread(file_path)
-    
+    img = cv2.imread(file_path)
+
     counter += 1
     print(counter)
 
     try:
-      dummy = img_ref.shape
+      dummy = img.shape
     except:
       print("Invalid image file while getting similarity: " + file_name)
 
-    img_ref_vect = get_feature_vector(img_ref)
-    score = calculate_similarity(img_vect, img_ref_vect)
-    if score > top1_score:
-      top1_image = img_ref
-      top1_path = file_path
-      top1_score = score
-    elif score > top2_score:
-      top2_image = img_ref
-      top2_path = file_path
-      top2_score = score
-    elif score > top3_score:
-      top3_image = img_ref
-      top3_path = file_path
-      top3_score = score
-  top_img_list = [top1_image, top2_image, top3_image]
-  top_path_list = [top1_path, top2_path, top3_path]
+    img_vect = get_feature_vector(img)
 
-  return top_img_list, top_path_list
+    with open('Dataset/feature_vectors/' + file_name + '.txt', 'w') as f:
+      for element in img_vect[0]:
+        f.write(str(element) + '\n')
 
-
-
+# Gets the vectory feature array from a text file
+def parse_feature_file(file_path):
+  with open(file_path) as f:
+    lines = f.read().splitlines()
+  x = np.array(lines)
+  y = x.astype(np.double)
+  y = y.reshape(1, 4096)
+  return y
+    
 
 
 
